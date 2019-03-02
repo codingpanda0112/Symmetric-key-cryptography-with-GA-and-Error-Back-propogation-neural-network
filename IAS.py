0e3479151ebfbc7f73c7a57f175e6fce581dc376
codingpanda0112 <nandinivineeth0112@gmail.com>
2019-03-02 14:08:05 +0530

    edited

--- IAS.py
+++ IAS.py
@@ -3,7 +3,20 @@ def Text_input():
 	String=input("Enter the  Plain-text:")
 	return String
 def Text_ascii(String):
-	convertText=' '.join(str(ord(c)) for c in String)
+	convertText=''
+	for c in String:
+		#print(c)
+		ascii_value= str(ord(c))
+		#print(ascii_value)
+		if len(ascii_value)<3:
+			pdded_Text='0'*(3-len(ascii_value))+ascii_value
+
+		else:
+			pdded_Text=ascii_value
+		#print(pdded_Text)
+		convertText=convertText+' '+pdded_Text
+		
+
 	return convertText
 
 
