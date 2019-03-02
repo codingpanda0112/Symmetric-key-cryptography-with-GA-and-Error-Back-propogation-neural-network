056e3b3a6d5407d89003bb6edea4a3eed6970cc5
codingpanda0112 <nandinivineeth0112@gmail.com>
2019-03-02 15:26:22 +0530

    edited

--- IAS.py
+++ IAS.py
@@ -2,6 +2,7 @@
 def Text_input():
 	String=input("Enter the  Plain-text:")
 	return String
+
 def Text_ascii(String):
 	convertText=''
 	for c in String:
@@ -15,11 +16,24 @@ def Text_ascii(String):
 			pdded_Text=ascii_value
 		#print(pdded_Text)
 		convertText=convertText+' '+pdded_Text
-		
-
 	return convertText
 
+def Ascii_binary(String):
+	
+	ascii_array=[]
+	bin_array=[]
+	print(String)
+	for  i in  range(len(String)):
+		print(type(i),i)
+		print(String[i],'ascii:',ord(String[i]))
+		ascii_array.append(ord(String[i]))
+		bin_array.append(bin(ascii_array[i])[2:].zfill(8))
+		
+	return ascii_array,bin_array
 
 string=Text_input()
 ct=Text_ascii(string)
-print(ct)
+#print(ct)
+a,b=Ascii_binary(string)
+print(a)
+print(b)
