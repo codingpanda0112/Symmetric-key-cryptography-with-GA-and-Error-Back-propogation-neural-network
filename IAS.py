96c74cf875a0c2556858a48c1b1c44a9821ade11
codingpanda0112 <nandinivineeth0112@gmail.com>
2019-03-09 12:42:45 +0530

    encryption function

--- IAS.py
+++ IAS.py
@@ -1,39 +1,143 @@
+import random
 
 def Text_input():
 	String=input("Enter the  Plain-text:")
+	String=String+'\n'+'\r'
 	return String
 
+def file_upload(filname):
+	string=''
+	file = open('filename', "r")
+	for line in file:
+		for char in line:
+			string+=char
+	return string		
+
 def Text_ascii(String):
+	
+	Acii_Array=[]
 	convertText=''
+
 	for c in String:
+		print(c)
 		#print(c)
 		ascii_value= str(ord(c))
 		#print(ascii_value)
 		if len(ascii_value)<3:
 			pdded_Text='0'*(3-len(ascii_value))+ascii_value
-
 		else:
 			pdded_Text=ascii_value
-		#print(pdded_Text)
+		#Acii_Array.append(pdded_Text)
 		convertText=convertText+' '+pdded_Text
-	return convertText
+	print(convertText)
+		#Acii_Array.append()
+	#print(Acii_Array)
+	
+	return Acii_Array
+def Ascii_binary(ascii_array):
+	bin_array=[]
+	for value in ascii_array:
+		for digit in value:
+			#print(bin(ord(digit))[2:].zfill(8))
+			bin_array.append(bin(ord(digit))[2:].zfill(8))
 
-def Ascii_binary(String):
+	return bin_array
+
+def Ascii_binary_func(String):
 	
 	ascii_array=[]
 	bin_array=[]
 	print(String)
 	for  i in  range(len(String)):
-		print(type(i),i)
-		print(String[i],'ascii:',ord(String[i]))
+		#print(type(i),i)
+		#print(String[i],'ascii:',ord(String[i]))
+
 		ascii_array.append(ord(String[i]))
 		bin_array.append(bin(ascii_array[i])[2:].zfill(8))
 		
 	return ascii_array,bin_array
 
-string=Text_input()
+def String_Split(bin_array):
+	joined=''.join(bin_array)
+	print(joined,'Length',len(joined))
+
+	mid=len(joined)//2
+	#mid=len(bin_array)//2
+	
+	Str1=""
+	Str2=""
+	String1=[]
+	String2=[]
+	print(mid)
+
+	for i in range(0,mid):
+		#print(bin_array[i])
+		Str1+=joined[i]
+				
+	for j in range(mid,len(joined)):
+		Str2+=joined[j]
+
+	String1=list(map(''.join, zip(*[iter(Str1)]*8)))
+	String2=list(map(''.join, zip(*[iter(Str2)]*8)))
+		
+	print('String1',String1)
+	print('String2',String2)
+
+	return String1,String2
+
+def Crossover(String1,String2):
+	joined_String=''.join(String1)
+	joined_String2=''.join(String2)
+
+	if len(String1)== len(String2):
+		for i in range(len(String1)//2+1):
+			j=i*2
+			String1[j],String2[j]=String2[j],String1[j]
+	return String1,String2
+
+def mutation(String1,rate):
+	number=rate*len(string1)*8
+	count=0
+	while(count<number):
+		j=random.random(0,len(String1))
+		k=random.random(0,8)
+		if(String1[j][k]==1):
+			String1[j][k]=0
+		elif (String1[j][k]==0):
+			String1[j][k]=1
+		count+=1
+
+	return String1
+
+def combine(String1,String2):
+	return String1+String2
+
+
+#string=Text_input()
+#string='This is the very important data'
+#string=file_upload(sampletext.txt)
 ct=Text_ascii(string)
-#print(ct)
-a,b=Ascii_binary(string)
-print(a)
-print(b)
+print(ct)
+bin_array=Ascii_binary(ct)
+print(bin_array)
+#a,b=Ascii_binary_func(string)
+#print(a)
+#print(b)
+string1,string2=String_Split(bin_array)
+'''
+print("String1")
+print(string1)
+print("String2")
+print(string2)
+print("=======================")
+print("After Crossover")
+String1,String2=Crossover(string1,string2)
+print("String1")
+print(String1)
+print("String2")
+print(String2)
+print("combined list")
+combinedlist=combine(String1,String2)
+print(combinedlist)
+string=mutation(combinedlist,30)'''
+
---
+++ test.py
@@ -0,0 +1,2 @@
+
+print(bin(ord('h')))
